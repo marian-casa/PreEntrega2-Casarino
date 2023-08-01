3532851d3a0c22eaae494f151a4d0f8ee4e9f706
Mariano Casarino <mariano.e.casarino@gmail.com>
2023-07-31 23:18:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+// Variables que llaman elementos del html con Id
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+// Agregar nuevas tareas
+function addTask() {
+    if (inputBox.value === '') {
+        alert('¡Tienes que escribir algo!')
+    } 
+    else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = 'x';
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+// funcion que permite marcar, desmarcar y eliminar tareas
+listContainer.addEventListener('click', function(e){
+    if (e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    } 
+    else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+// Con estas funciones guardamos los datos ingresados
+function saveData() {
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+showTask();
